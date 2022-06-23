6ba0f5c5736a8f47b952d170fd9377f425375fbd
StarMac <starmac2008@gmail.com>
2022-06-23 18:00:18 +0300

    added min index (lab 2 java)

--- Lab 2/Java/PCP_Lab2/src/com/company/FindMin.java
+++ Lab 2/Java/PCP_Lab2/src/com/company/FindMin.java
@@ -2,12 +2,15 @@ package com.company;
 
 public class FindMin {
     private int min = Integer.MAX_VALUE;
+    private int minIndex = -1;
 
         public void FindMin(int[] arr, int threadsCount) throws java.lang.Exception
         {
             Thread[] threads = new Thread[threadsCount];
 
             min = Integer.MAX_VALUE;
+
+            minIndex = -1;
 
             for (int threadId = 0; threadId < threads.length; threadId++)
             {
@@ -28,13 +31,14 @@ public class FindMin {
                 thread.join();
             }
 
-            System.out.println("Number of threads: " + threadsCount + ", min: " + min);
+            System.out.println("Number of threads: " + threadsCount + ", min: " + min + ", min index:" + minIndex);
         }
 
         synchronized private void setMin(int[] arr, int index){
             if (arr[index] < min)
             {
                 min = arr[index];
+                minIndex = index;
             }
         }
 }
